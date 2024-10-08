51b87c0544f4da3d37553e99f8ee7439b4df89d0
Jacob Dahl <dahl.jakejacob@gmail.com>
2024-10-07 18:15:09 -0800

    service: flight_review: update for changed paths

--- platform/common/scripts/start_flight_review.sh
+++ platform/common/scripts/start_flight_review.sh
@@ -2,4 +2,4 @@
 HOSTNAME="$(hostname -f).local"
 PORT=5006
 
-python3 /opt/flight_review/app/serve.py --port=$PORT --address=0.0.0.0 --use-xheaders --allow-websocket-origin=$HOSTNAME --allow-websocket-origin=$HOSTNAME:$PORT
+python3 ~/.local/share/flight_review/app/serve.py --port=$PORT --address=0.0.0.0 --use-xheaders --allow-websocket-origin=$HOSTNAME --allow-websocket-origin=$HOSTNAME:$PORT
