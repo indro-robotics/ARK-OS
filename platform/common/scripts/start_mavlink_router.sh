170bd1b1d7f6589794e07e04fe05d231a0c338a3
Jacob Dahl <dahl.jakejacob@gmail.com>
2024-10-15 09:57:53 -0800

    fix start_mavlink_router.sh to only update FC USB device path in main.conf

--- platform/common/scripts/start_mavlink_router.sh
+++ platform/common/scripts/start_mavlink_router.sh
@@ -11,8 +11,8 @@ if [ -z "$DEVICE_PATH" ]; then
     exit 1
 fi
 
-# Update the main.conf file with the correct device path for FCUSB
-sed -i "s|^Device =.*|Device = $DEVICE_PATH|" "$MAVLINK_ROUTERD_CONF_FILE"
+# Replace serial device "*if00" with correct device path for PX4/Ardupilot
+sed -i "s|^Device =.*if00|Device = $DEVICE_PATH|" "$MAVLINK_ROUTERD_CONF_FILE"
 
 # Enable mavlink usb stream first
 python3 ~/.local/bin/vbus_enable.py
