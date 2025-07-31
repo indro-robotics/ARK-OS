119d8ab1a1ecac831a96ff07d30b262149c524e4
Muaz-InDro <muaz.khalid@indrorobotics.com>
2025-07-31 15:02:56 -0400

    Build Micro-XRCE dds agent from source rather than via snap

--- scripts/install_dds_agent.sh
+++ scripts/install_dds_agent.sh
@@ -5,7 +5,16 @@ echo "Installing micro-xrce-dds-agent"
 
 service_uninstall dds-agent
 
-sudo snap install micro-xrce-dds-agent --edge
+#sudo snap install micro-xrce-dds-agent --edge
+
+cd
+sudo snap remove micro-xrce-dds-agent #Removing the snap restricted version of the DDS-Agent
+git clone https://github.com/eProsima/Micro-XRCE-DDS-Agent.git #Building DDS-Agent from Source
+cd Micro-XRCE-DDS-Agent
+mkdir build && cd build
+cmake ..
+make
+sudo make install
 
 service_add_manifest dds-agent
 
